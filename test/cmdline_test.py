d1aa77069ca5aeee3332d9545bf3c28115c6f404
yuanchang <yuanchang.akm@gmail.com>
2018-08-16 14:10:24 +0900

    modified

--- test/cmdline_test.py
+++ test/cmdline_test.py
@@ -2,36 +2,64 @@ import unittest, logging, argparse, os, datetime, subprocess
 from pathlib import Path
 
 basePath = os.path.abspath(os.path.dirname(__file__))
-envpath = './fratest-output'
+envpathout = './cmdline_test_output'
+envpathin = './cmdline_test_input'
+
 totalLogs = ''
-fraui_path = os.path.join(Path(basePath).parent, 'src/fraui.py')
+fraui_path = os.path.join(Path(basePath).parent, './src/fraui.py')
+topurllistPath = os.path.join(envpathin, 'fract.akamaized.net_urls.csv')
+urllistPath = os.path.join(envpathout, 'urllist.txt')
+
+if not os.path.isdir(envpathout):
+    os.mkdir(envpathout)
+if not os.path.isdir(envpathin):
+    os.mkdir(envpathin)
 
 class testFractCommnand(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.fraui_path = fraui_path
         self.command = ''
-        if (os.path.isfile('./urllist.txt')):
-            os.remove('./urllist.txt')
 
     @classmethod
     def tearDownClass(self):
         pass
 
+    def setUp(self):
+        if (os.path.isfile(urllistPath)):
+            os.remove(urllistPath)
+
     def test_HelpInformation(self):
+        logging.info('Testing: Help Information Output')
         self.command = ['python3', fraui_path]
         result = subprocess.run(self.command, stdout=subprocess.PIPE)
         stdout_org = result.stdout.decode('utf-8')
-        logging.info('Testing Help Information')
         self.assertIn('usage: fract [-h] [-v]', stdout_org)
 
     def test_GetURLc(self):
-        self.command = ['python3', fraui_path, 'geturlc', '-e', 'https://www.uniqlo.com/', '-d', '1', '-o', 'urllist.txt', '-D', 'www.uniqlo.com']
+        logging.info('Testing: URL generation')
+        self.command = ['python3', fraui_path, 'geturlc', '-e', 'https://fract.akamaized.net/', '-d', '1', '-o', urllistPath, '-D', 'fract.akamaized.net']
         subprocess.run(self.command, stdout=subprocess.PIPE)
-        self.assertTrue(os.path.isfile('./urllist.txt'))
+        self.assertTrue(os.path.isfile(urllistPath))
+        if os.path.isfile(urllistPath):
+            with open(urllistPath, mode='r') as rf:
+                contents = rf.read()
+                self.assertTrue(contents.index('https://fract.akamaized.net/css/main.css') > 0)
+
+    def test_GetURLcFromCSV(self):
+        logging.info('Testing: URL generation by Akamai top url')
+        self.command = ['python3', fraui_path, 'geturlakm', '-i', topurllistPath, '-D', 'fract.akamaized.net', '-p', 'https', '-o', urllistPath]
+        subprocess.run(self.command, stdout=subprocess.PIPE)
+        self.assertTrue(os.path.isfile(urllistPath))
+        if os.path.isfile(urllistPath):
+            with open(urllistPath, mode='r') as rf:
+                contents = rf.read()
+                self.assertTrue(contents.index('https://fract.akamaized.net/css/main.css') > 0)
+
+    def test_Maketestcases(self):
+        pass
 
 if __name__ == '__main__':
     print('Version 0.1')
     logging.basicConfig(level=logging.DEBUG)
     unittest.main()
-    print('All Process End')

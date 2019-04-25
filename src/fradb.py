254a7f7ca7563441fe6424ff826e2ad38f1f0f73
yuanchang <yuanchang.akm@gmail.com>
2019-04-25 16:27:14 +0900

    Initial structure

--- src/fradb.py
+++ src/fradb.py
@@ -1,3 +1,25 @@
 import pymongo
 
-dbclient = pymongo.MongoClient('localhost', 27017)
+class Mongo():
+    '''
+    Make Mongo connection instance
+    '''
+    __instance = None
+
+    def __init__(self):
+        self.mdb_client = pymongo.MongoClient('localhost', 27017)
+        print('Connecting Mongo...')
+
+    @staticmethod
+    def getInstance():
+        if Mongo.__instance is None:
+            Mongo.__instance = Mongo()
+        return Mongo.__instance
+
+class mongojson():
+    def input(self, jsonsource):
+        pass
+
+    def output(self, outputpath):
+        pass
+    

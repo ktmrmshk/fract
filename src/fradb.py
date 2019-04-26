f4b00912e37116b0b838392c6923aee07cfc5152
yuanchang <yuanchang.akm@gmail.com>
2019-04-26 15:34:43 +0900

    Update 0426_01

--- src/fradb.py
+++ src/fradb.py
@@ -1,4 +1,6 @@
 import pymongo
+import os
+import json
 
 class Mongo():
     '''
@@ -17,9 +19,30 @@ class Mongo():
         return Mongo.__instance
 
 class mongojson():
-    def input(self, jsonsource):
-        pass
+    def input(self, jsonSourceFile, dbName, collectionName):
+        mongoInstance = Mongo.getInstance()
+        if os.path.isfile(jsonSourceFile):
+            mongodb = mongoInstance.mdb_client[dbName]
+            collection = mongodb[collectionName]
+            with open(jsonSourceFile, mode = 'r') as jf:
+                str = jf.read()
+                jsonData = []
+                jsonData.extend(json.loads(str))
+                collection.insert_many(jsonData)
+        else:
+            print('{} is not exist'.format(jsonSourceFile))
+        
 
-    def output(self, outputpath):
-        pass
-    
+    def output(self, outputpath, dbName, collectionName, includeObjID = False):
+        mongoInstance = Mongo.getInstance()
+        mongodb = mongoInstance.mdb_client[dbName]
+        collection = mongodb[collectionName]
+        jsonOutput = []
+        if includeObjID == True:
+            for i in collection.find():
+                jsonOutput.append(i)
+        else:
+            for i in collection.find({}, {'_id': False}):
+                jsonOutput.append(i)
+        with open(outputpath, mode = 'w') as jf:
+            jf.write(json.dumps(jsonOutput, indent=4))

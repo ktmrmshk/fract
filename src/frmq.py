9fda28c5a0ba13a75ff63c00374a861aab2ac4cf
Masahiko Kitamura <kitamura@Masahikos-MacBook-Pro.local>
2019-06-15 09:43:44 +0900

    split fractworker

--- src/frmq.py
+++ src/frmq.py
@@ -1,8 +1,4 @@
 import pika, json
-from functools import partial
-from fract import *
-from frase import *
-from fradb import *
 import socket, time
 
 
@@ -53,93 +49,8 @@ class TaskPublisher(RabbitMQMan):
     def push(self, queuename, body):
         self.ch.basic_publish(exchange='', routing_key=queuename, body=body, properties=pika.BasicProperties(delivery_mode=2))
 
-class TaskWorker(RabbitMQMan):
-    def callback(self, ch, method, properties, body):
-        'must implement in child class'
-        pass
-
-    def consume(self, queuename):
-        self.ch.basic_qos(prefetch_count=1)
-        self.ch.basic_consume(queue=queuename, on_message_callback=self.callback)
-        self.ch.start_consuming()
-    
-
-class SimpleDumpWorker(TaskWorker):
-    def callback(self, ch, method, properties, body):
-        logging.debug(body)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-class FractWorker(TaskWorker):
-    def __init__(self):
-        '''
-        fract_sub = {'testgen': callback_for_testgen, 'run': callback_for_run}
-        '''
-        super(FractWorker, self).__init__()
-        self.fract_sub = {}
-    
-    def addCallback(self, cmd, callback):
-        '''
-        callback: function(msg)
-        dumper: output adaptor function
-        '''
-        self.fract_sub[cmd] = partial(callback)
-
-    def callback(self, ch, method, properties, body):
-        msg = json.loads(body)
-        cmd = msg['cmd']
-        self.fract_sub[cmd](msg)
-        ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    def pull_single_msg(self, queuename):
-        method, properties, body = self.ch.basic_get(queue=queuename, auto_ack=True)
-        
-        msg = json.loads(body)
-        cmd = msg['cmd']
-        self.fract_sub[cmd](msg)
-        self.ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-
-class FractSubtask(object):
-    @staticmethod
-    def do_task(msg):
-        pass
-
-class Subtask_TestGen(FractSubtask):
-    @staticmethod
-    def do_task(msg):
-        assert msg['cmd'] == 'testgen'
-        logging.debug('sub_testgen: msg => {}'.format(msg))
-        
-        fg = FraseGen()
-        fg._gen_from_urllist(msg['urllist'], msg['src_ghost'], msg['dst_ghost'], msg['headers'], msg['options'], msg['mode'])
-
-        # export results to mongo
-        mj=mongojson()
-        mj.push_many(fg.testcases, msg['cmd'], msg['sessionid'], lambda i : i.query)
-
-class Subtask_Run(FractSubtask):
-    @staticmethod
-    def do_task(msg):
-        assert msg['cmd'] == 'run'
-        logging.debug('sub_run: msg => {}'.format(msg))
-
-        testcases = msg['testcases']
-        fclient = FractClient(fract_suite_json=json.dumps(testcases) )
-        fclient.run_suite()        
-
-        # export results to mongo
-        mj=mongojson()
-        if len(fclient._result_suite) > 0:
-            # resultl = list()
-            # for node_result in fclient._result_suite:
-            #     resultl.append(node_result.__str__())
-            mj.push_many(fclient._result_suite, msg['cmd'], msg['sessionid'], lambda i : i.query)
-        if len(fclient._failed_result_suite) > 0:
-            # resultl = list()
-            # for node_result in fclient._result_suite:
-            #     resultl.append(node_result.__str__())
-            mj.push_many(fclient._failed_result_suite, msg['cmd'], msg['sessionid'] + '_failed', lambda i : i.query)
 
 
 
--- test/test_frmq.py
+++ test/test_frmq.py
@@ -13,6 +13,7 @@ from frmq import *
 from fradb import *
 from config import CONFIG
 from fractman import *
+from fractworker import *
 
 class test_MQMan(unittest.TestCase):
     def setUp(self):
@@ -78,28 +79,6 @@ class test_TaskPublisher(unittest.TestCase):
         self.tp.make_queue('kitaq2')
         self.tp.push('kitaq2', 'foobar123')
         
-class test_TaskWorker(unittest.TestCase):
-    def setUp(self):
-        self.tw = TaskWorker()
-        self.tw.open(CONFIG['mq']['host'], CONFIG['mq']['port'])
-
-    def tearDown(self):
-        self.tw.close()
-    def test_callbackd(self):
-        pass
-    #def test_comsume(self):
-    #    self.tw.comsume('kitaq')
-
-class test_SimpleDumpWorker(unittest.TestCase):
-    def setUp(self):
-        self.sdw = SimpleDumpWorker()
-        self.sdw.open(CONFIG['mq']['host'], CONFIG['mq']['port'])
-
-    def tearDown(self):
-        self.sdw.close()
-    def test_consume(self):
-        #self.sdw.consume('kitaq')
-        pass
 
 class test_TestGenPublisher(unittest.TestCase):
     def setUp(self):
@@ -137,72 +116,6 @@ class test_TestGenPublisher(unittest.TestCase):
         publisher.push('fractq2', '12345678', urllist, 'e13100.a.akamaiedge.net', 'e13100.a.akamaiedge-staging.net')
 
         
-
-class test_FractWorker(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-
-
-    def test_sub_testgen_with_mongo_dumper(self):
-        # publish testgen
-        urllist=['https://space.ktmrmshk.com/', 'https://space.ktmrmshk.com/js/mobile.js']
-        publisher = TestGenPublisher(CONFIG['mq']['host'], CONFIG['mq']['port'])
-        publisher.push('fractq', '1981121111', urllist, 'e13100.a.akamaiedge.net', 'e13100.a.akamaiedge-staging.net')
-
-        ### spawn worker
-        worker = FractWorker()
-        worker.open(CONFIG['mq']['host'], CONFIG['mq']['port'])
-
-        ## Clean up collection
-        mj = mongojson()
-        mj.clean('testgen', '1981121111')
-
-
-        worker.addCallback('testgen', Subtask_TestGen.do_task)
-        #worker.consume('fractq')
-        worker.pull_single_msg('fractq')
-        
-        # check
-        ret = mj.find({}, 'testgen', '1981121111')
-        self.assertTrue( len(ret) == 2 )
-
-        mj.clean('testgen', '1981121111')
-
-    def test_subtask_run(self):
-        
-        ### spawn worker
-        worker = FractWorker()
-        worker.open(CONFIG['mq']['host'], CONFIG['mq']['port'])
-        worker.addCallback('run', Subtask_Run.do_task)
-
-
-        # push msg
-        sessionid = str(random.random())
-        rm=RunMan(sessionid)
-
-        rm.pub.purge(CONFIG['mq']['queuename'])
-        chunksize=5
-        rm.push_testcase_from_file('testcase4test.json', chunksize)
-        
-        # count number of testcase in this testcases file
-        num_test=0
-        with open('testcase4test.json') as f:
-            tests = json.load(f)
-            num_test=len(tests)
-               
-        # pull msg and do task
-        worker.pull_single_msg(CONFIG['mq']['queuename'])
-        
-        # check the testresult
-        mj = mongojson()
-        
-        ret = mj.find({}, 'run', sessionid)
-        self.assertTrue( len(ret) == chunksize )
-
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     unittest.main()
